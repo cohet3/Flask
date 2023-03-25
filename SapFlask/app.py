b679b133cc5e78310842aae6bb6704e335b173d7
cohet3 <tauro.total90@gmail.com>
2023-03-25 13:36:09 +0100

    commit

--- SapFlask/app.py
+++ SapFlask/app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 # Configuracionde la bd
 USER_DB = 'postgres'
-PASS_DB = 'David2020'
+PASS_DB = 'Deivid_2020'
 URL_DB = 'localhost'
 NAME_DB = 'sap_flask_db'
-FULL_URL_DB = f'postgresl://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,9 +23,9 @@ migrate.init_app(app, db)
 
 class Persona(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Colimn(db.String(250))
-    apellido = db.Colimn(db.String(250))
-    email = db.Colimn(db.String(250))
+    nombre = db.Column(db.String(250))
+    apellido = db.Column(db.String(250))
+    email = db.Column(db.String(250))
 
     def __str__(self):
         return (
