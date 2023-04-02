0a0e4d024216a7e7d564c4d146d2349078458159
cohet3 <tauro.total90@gmail.com>
2023-04-02 22:35:02 +0200

    commit

--- SapFlask/app.py
+++ SapFlask/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -20,6 +23,8 @@ db = SQLAlchemy(app)
 # configurar flask-migrate
 migrate = Migrate()
 migrate.init_app(app, db)
+#configuracion de flask-wtf
+app.config['SECRET_KEY']='vamos_a_tope_key'
 
 
 class Persona(db.Model):
@@ -35,7 +40,11 @@ class Persona(db.Model):
             f'Apellido: {self.apellido}'
             f'Email: {self.email}'
         )
-
+class PersonaForm(FlaskForm):
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    apellido = StringField('Apellido')
+    email = StringField('Email', validators=[DataRequired()])
+    enviar = SubmitField('Enviar')
 
 @app.route('/')
 @app.route('/index')
@@ -47,3 +56,25 @@ def inicio():
     app.logger.debug(f'Listado Personas: {personas}')
     app.logger.debug(f'Total Personas: {total_personas}')
     return render_template('index.html', personas=personas, total_personas=total_personas)
+
+@app.route('/ver/<int:id>')
+def ver_detalle(id):
+    #Recuperamos la persona según el id proporcionado
+    #persona = Persona.query.get(id)
+    persona = Persona.query.get_or_404(id)
+    app.logger.debug(f'Ver persona: {persona}')
+    return render_template('detalle.html', persona=persona)
+
+@app.route('/agregar', methods=['GET','POST'])
+def agregar():
+    persona = Persona()
+    personaForm = PersonaForm(obj=persona)
+    if request.method == 'POST':
+        if personaForm.validate_on_submit():
+           personaForm.populate_obj(persona)
+           app.logger.debug(f'Persona a insertar: {persona}')
+           #Insertamos el nuevo registro
+           db.session.add(persona)
+           db.session.commit()
+           return redirect(url_for('inicio'))
+    return render_template('agregar.html', forma=personaForm)
