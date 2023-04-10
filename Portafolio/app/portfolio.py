486594b5b374647b8eeadb6a2dd89c0449bf0654
cohet3 <tauro.total90@gmail.com>
2023-04-10 16:53:01 +0200

    commit

--- Portafolio/app/portfolio.py
+++ Portafolio/app/portfolio.py
@@ -4,3 +4,6 @@ bp = Blueprint('portfolio', __name__, url_prefix='/')
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('portfolio/index.html')
+@bp.route('/mail', methods=['POST'])
+def mail():
+    return 'lala'
